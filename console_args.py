96364a7eec61b44788439b2e46f133434d2c7879
Ja <Ja_Pan_Krzysztof@gmail.com>
2022-04-22 21:12:49 +0100

    create a project

---
+++ console_args.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(
+        prog='listening',
+        description='Copyright by Ja',
+        # usage='%(prog)s [option] function firstPath lastPath',
+        epilog='have you a nice day :)',
+        fromfile_prefix_chars='@',  # inportowanie argumentów z pliku txt
+        allow_abbrev=False,  # pisanie skrutowo argumentów
+    )
+    arg_parser.add_argument(
+        'Start_Path',
+        metavar='first_path',
+        type=str,
+        help='The path to first folder'
+    )
+    arg_parser.add_argument(
+        'End_Path',
+        metavar='last_path',
+        type=str,
+        help='The path to last folder'
+    )
+    arg_parser.add_argument(
+        '-i',
+        '--infinity',
+        action='store_true',
+        help='set time update [s]'
+    )
+    args = arg_parser.parse_args()
+
+    return args
+
+
+'''    if not os.path.isdir(path_input):
+        print('Nie podano ścieżki :(')
+        sys.exit()
+
+    print('\n'.join(os.listdir(path_input)))
+'''
+
---
+++ listening.py
@@ -0,0 +1,79 @@
+from shutil import copytree
+from threading import Thread
+import os
+from time import sleep, perf_counter
+
+import console_args
+
+
+class Listening:
+    def __init__(self, path_start: str = ..., path_end: str = ...):
+        self.path_start = path_start
+        self.path_end = path_end
+
+    @staticmethod
+    def copy(path1, path2):
+        print('Działa')
+        copytree(path1, path2, dirs_exist_ok=True)
+
+    def copy_to_folder(self):
+        print(f'''  Copy file :
+        [from] \t {self.path_start}
+        [to] \t {self.path_end}
+        ''')
+
+        files = os.listdir(self.path_start)
+
+        for file in files:
+            filename, extention = os.path.splitext(file)
+            extention = extention[1:]
+
+            if extention == '':
+                t1 = Thread(target=self.copy, args=(self.path_start + f'/{filename}', self.path_end + f'/{filename}'))
+                t1.start()
+
+            else:
+                copytree(self.path_start, self.path_end, dirs_exist_ok=True)
+
+    def listening_on_folder(self):
+        print(f'''  Listening to : 
+        [from] \t {self.path_start}
+        [to] \t {self.path_end}
+        ''')
+
+        start = perf_counter()
+
+        try:
+            while True:
+                copytree(self.path_start, self.path_end, dirs_exist_ok=True)
+
+                sleep(2)
+                continue
+
+            end = perf_counter()
+
+            print(f'Listening continued : {round(end - start, 2)}s.')
+            return 0
+
+        except KeyboardInterrupt:
+            end = perf_counter()
+            print(f'Listening continued : {round(end - start, 2)}s.')
+
+
+def main():
+    args = console_args.main()
+
+    listening = Listening(args.Start_Path, args.End_Path)
+    match args.infinity:
+        case False:
+            listening.copy_to_folder()
+
+        case True:
+            listening.listening_on_folder()
+
+        case _:
+            print('Ale z ciebie idiota jest :)')
+
+
+if __name__ == '__main__':
+    main()
